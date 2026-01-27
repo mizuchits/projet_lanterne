449c982c2328f882507b14fc653c32fa12f959ea
mizuchits <marin.tom65@gmail.com>
2026-01-27 10:30:31 +0100

    fix + edit dashboard + contact

--- src/Controller/Admin/LanterneCrudController.php
+++ src/Controller/Admin/LanterneCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Lanterne;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +16,14 @@ class LanterneCrudController extends AbstractCrudController
         return Lanterne::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('name'),
+            TextField::new('spe'),
+            IntegerField::new('prix'),
             TextEditorField::new('description'),
         ];
     }
-    */
 }
